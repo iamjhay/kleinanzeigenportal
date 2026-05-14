7278d59b0d8cdf603ee29b4ef7902e65bd4fcfe1
iamjhay <jhaydrille75@gmail.com>
2026-05-14 17:31:36 +0100

    refactor: reorder middleware initialization and update routing matcher configuration

--- middleware.ts
+++ middleware.ts
@@ -3,19 +3,19 @@ import { routing } from "./i18n/routing";
 import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
-const { auth } = NextAuth(authConfig);
 const intlMiddleware = createMiddleware(routing);
+const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
   return intlMiddleware(req);
 });
 
 export const config = {
-  // Match only internationalized pathnames and auth paths
-  // Exclude static assets, images, and api routes
+  // Matcher for next-intl and next-auth
   matcher: [
-    "/", 
-    "/(de|en)/:path*", 
-    "/((?!api|_next/static|_next/image|assets|favicon.ico|.*\\..*).*)"
+    // Skip all internal paths (_next) and static files
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|.*\\..*).*)",
+    // Always run on the root
+    "/",
   ],
 };
